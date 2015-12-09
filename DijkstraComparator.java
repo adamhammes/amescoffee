26726554c37a6e9b1bc1aa0cd27c599ca27616bc
Adam Hammes <adam@hammes.io>
2015-12-08 19:03:28 -0600

    Renamed cost -> costs

--- DijkstraComparator.java
+++ DijkstraComparator.java
@@ -4,14 +4,14 @@ import java.util.Comparator;
 import java.util.Map;
 
 public class DijkstraComparator<S, T> implements Comparator<Vertex<S, T>> {
-    private Map<Vertex<S, T>, Double> cost;
+    private Map<Vertex<S, T>, Double> costs;
 
-    public DijkstraComparator(Map<Vertex<S, T>, Double> cost) {
-        this.cost = cost;
+    public DijkstraComparator(Map<Vertex<S, T>, Double> costs) {
+        this.costs = costs;
     }
 
     @Override
     public int compare(Vertex<S, T> v1, Vertex<S, T> v2) {
-        return Double.compare(cost.get(v1), cost.get(v2));
+        return Double.compare(costs.get(v1), costs.get(v2));
     }
 }
