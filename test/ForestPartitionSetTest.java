a4879591b779412242dc4c380bf6b5b4ab1d83d7
Adam Hammes <adam@hammes.io>
2015-12-08 20:32:27 -0600

    Created initial test cases

--- test/ForestPartitionSetTest.java
+++ test/ForestPartitionSetTest.java
@@ -2,6 +2,9 @@ package cs311.hw7.test;
 
 import cs311.hw7.ForestPartitionSet;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,27 +15,27 @@ public class ForestPartitionSetTest {
 
     @org.junit.Before
     public void setup() {
-
+        List<Integer> initialElements = Arrays.asList(new Integer[] { 1, 2, 3, 4 });
+        pSet = new ForestPartitionSet<>(initialElements);
     }
 
 
     @org.junit.Test
-    public void testFind() throws Exception {
-
+    public void testSimpleFind() throws Exception {
+        assertEquals(new Integer(1), pSet.find(1));
     }
 
     @org.junit.Test
-    public void testMakeSet() throws Exception {
-
+    public void testSimpleMakeSet() throws Exception {
+        pSet = new ForestPartitionSet<>();
+        pSet.makeSet(1);
+        assertEquals(new Integer(1), pSet.find(1));
     }
 
     @org.junit.Test
-    public void testUnion() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testInSameSet() throws Exception {
-
+    public void testUnionAndInSameSet() throws Exception {
+        assertFalse(pSet.inSameSet(1, 2));
+        pSet.union(1, 2);
+        assertTrue(pSet.inSameSet(1, 2));
     }
 }
