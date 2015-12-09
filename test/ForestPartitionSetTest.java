82ad4b3ab5c34658cf14ba0da5a573d0eed1bb52
Adam Hammes <adam@hammes.io>
2015-12-08 20:23:53 -0600

    Created test directory

---
+++ test/ForestPartitionSetTest.java
@@ -0,0 +1,38 @@
+package cs311.hw7.test;
+
+import cs311.hw7.ForestPartitionSet;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by adam on 12/8/15.
+ */
+public class ForestPartitionSetTest {
+    ForestPartitionSet<Integer> pSet;
+
+    @org.junit.Before
+    public void setup() {
+
+    }
+
+
+    @org.junit.Test
+    public void testFind() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testMakeSet() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUnion() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testInSameSet() throws Exception {
+
+    }
+}
